b8cf4813b0f11499b1840878b80f93b46bdaf473
priyanshub18 <priyanshubhardwaj658@gmail.com>
2025-02-02 12:32:49 +0530

    image-fix

--- src/view-trip/components/Hotel.jsx
+++ src/view-trip/components/Hotel.jsx
@@ -13,7 +13,7 @@ function Hotel({ trip }) {
   return (
     <div>
       <h2 className="font-bold text-xl mt-5 ">Hotel Recommendations</h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 ">{!loading ? trip?.tripData?.hotelOptions.map((item, index) => <HotelCard item={item} index={index} />) : [1, 2, 3].map((item, index) => <div key={index} className="h-[300px] w-full bg-slate-200 animate-pulse rounded-xl"></div>)}</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5 ">{!loading ? trip?.tripData?.hotelOptions?.map((item, index) => <HotelCard item={item} index={index} />) : [1, 2, 3].map((item, index) => <div key={index} className="h-[300px] w-full bg-slate-200 animate-pulse rounded-xl"></div>)}</div>
     </div>
   );
 }
--- src/view-trip/components/PlacesToVisit.jsx
+++ src/view-trip/components/PlacesToVisit.jsx
@@ -7,12 +7,12 @@ function PlacesToVisit({ trip }) {
     <div>
       <h2 className="font-bold text-lg mt-5 ">Places to Visit</h2>
       <div>
-        {trip?.tripData?.itinerary.map((item, index) => (
+        {trip?.tripData?.itinerary?.map((item, index) => (
           <div key={index}>
             <h2 className="font-bold text-xl mt-5 ">DAY {item?.day}</h2>
             <div className="grid  grid-cols-1 md:grid-cols-2 gap-4 ">
               {/* <h2>{item?.bestTimeToVisit}</h2> */}
-              {item.places.map((place, index) => (
+              {item?.places.map((place, index) => (
                 <div key={index} className="my-3  border border-gray-100 hover:border-black  hover:border-2 transition-all duration-200 p-4 rounded-lg  ">
                   <h2 className="font-medium text-sm text-orange-400  mx-2">Time To Travel: {place.timeToTravel}</h2>
                   <PlaceCardItem place={place} />
